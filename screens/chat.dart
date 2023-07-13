3a89a2299a8978c0f3dab0f4b4d996d1ac53f2be
sahDipti <123860643+sahDipti@users.noreply.github.com>
2023-07-13 23:13:48 +0545

    Add files via upload

---
+++ screens/chat.dart
@@ -0,0 +1,58 @@
+import 'package:chat/widgets/chat_messages.dart';
+import 'package:chat/widgets/new_messages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget{
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() {
+    return _ChatScreenState();
+  }
+
+}
+
+class _ChatScreenState extends State<ChatScreen>{
+
+  void setupPushNotifications() async{
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+    
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              FirebaseAuth.instance.signOut();
+            }, 
+            icon: Icon(Icons.exit_to_app,
+            color: Theme.of(context).colorScheme.primary,
+            )
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages()
+            ),
+          NewMessage()
+        ],),
+      );
+  }
+}
